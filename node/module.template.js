58b9748dc360e265ea0780895c55fd62d3a64ac2
arthur791004 <arthur791004@gmail.com>
2016-10-13 11:58:44 +0800

    Create module.template.js

---
+++ node/module.template.js
@@ -0,0 +1,41 @@
+/**
+ * Module exports
+ */
+module.exports = (function() {
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+  const winston = require('winston');
+
+  /**
+   * Global Constants
+   */
+  const LOGGER = new winston.Logger({
+    transports: [
+      new winston.transports.File({
+        filename: '<logfile>',
+        level: process.env.DEBUG ? 'debug' : 'info',
+        prettyPrint: true,
+        handleExceptions: true,
+        json: false,
+        colorize: true
+      })
+    ],
+    exitOnError: false
+  });
+
+  /**
+   * Class
+   */
+  class <ModuleName> {
+    constuctor() {
+      // do something...
+    }
+  }
+
+  /**
+   * Functions
+   */
+})();
