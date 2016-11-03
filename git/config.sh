0765302876ddf971a315d9671a789676fbd562e3
arthur791004 <arthur791004@gmail.com>
2016-11-03 14:40:29 +0800

    Update config.sh

--- git/config.sh
+++ git/config.sh
@@ -3,3 +3,4 @@ git config --global user.name \<name>
 git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.br branch
+git config --global alias.ap "add --patch"
