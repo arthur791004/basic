d3dba6fe5adace5203ec82581fc3acd29f675c95
arthur791004 <arthur791004@gmail.com>
2016-11-03 10:41:01 +0800

    Create config.sh

---
+++ git/config.sh
@@ -0,0 +1,5 @@
+git config --global user.email \<email>
+git config --global user.name \<name>
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br branch
