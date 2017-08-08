96763eab15cdfef521aae78f3314cf988a761f26
arthur791004 <arthur791004@gmail.com>
2017-08-08 14:15:00 +0800

    Update config.sh

--- source_control/git/config.sh
+++ source_control/git/config.sh
@@ -4,4 +4,5 @@ git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.ap "add --patch"
+git config --global alias.unstage 'reset HEAD --'
 git config --global credential.helper "cache --timeout=86400"
